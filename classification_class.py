b1e6a8587d4c5441190a2f88c03dd41652aa18bf
Jason <jason_morgan84@yahoo.co.uk>
2026-03-27 12:12:26 +0000

    working training

--- classification_class.py
+++ classification_class.py
@@ -1,24 +1,57 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import pandas as pd
-import torch
 from skimage import io
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import torch
 
 class ImagesDataset(Dataset):
 
-    def __init__(self, data_file, directory):
+    def __init__(self, annotations_file, img_dir,transform=None, target_transform=None):
 
-        self.image_data = pd.read_csv(directory + data_file,header=None)
-        self.directory = directory
+        self.img_labels = pd.read_csv(img_dir + annotations_file,header=None)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.image_data)
+        return len(self.img_labels)
 
-    def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.tolist()
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        #reads image - uses io.imread because its a tif, also transposes to proper order for conversion to tensor
+        image = io.imread(img_path).transpose(1,2,0)
+        label = torch.tensor(self.img_labels.iloc[idx, 1])
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+    
+    
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=2, out_channels=8, kernel_size=5, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=5, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(8)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv4 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(16)
+        self.conv5 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=5, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(16)
+        self.fc1 = nn.Linear(16*122*122, 10)
 
-        img_name = self.directory + self.image_data.iloc[index, 0]
-        image = io.imread(img_name)
-        type = self.image_data.iloc[index, 1]
-        sample = {'image': image, 'type': type}
-        return sample
+    def forward(self, input):
+        output = F.relu(self.bn1(self.conv1(input)))   
+        output = F.relu(self.bn2(self.conv2(output)))     
+        output = self.pool(output)                   
+        output = F.relu(self.bn4(self.conv4(output)))     
+        output = F.relu(self.bn5(self.conv5(output)))     
+        output = output.view(-1, 16*122*122)
+        output = self.fc1(output)
+
+        return output
