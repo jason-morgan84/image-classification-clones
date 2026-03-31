45afca4a9fe95753e1911f7f7fd0bf4ca559c620
jason <jason_morgan84@yahoo.co.uk>
2026-03-31 14:58:04 +0100

    added accuracy by group

--- imageclassification.py
+++ imageclassification.py
@@ -1,15 +1,14 @@
 import torch
-#import pandas as pd
-import matplotlib.pyplot as plt
+from sympy import true
+
 import classification_class
-from torchvision.transforms import ToTensor,Compose,Normalize
+from torchvision.transforms import Compose,Normalize
 import torchvision
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.autograd import Variable
 import time
-import numpy as np
 #from captum.attr import Occlusion
 
 #-1: Fix artifacts in outputted images?
@@ -26,14 +25,8 @@ import numpy as np
 batch_size=5
 classes = ("AR","ARS")
 
-def testImage(index):
-    model.eval()
-
-    image, label = test_images[index]
-    print(label)
-
 # Function to test the model with a batch of images and show the labels predictions
-def testBatch():
+def test_batch():
     # get batch of images from the test DataLoader
     model.eval()
     images, labels = next(iter(test_loader))
@@ -49,7 +42,8 @@ def testBatch():
     print('Real labels: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
 
     # Let's see what if the model identifiers the  labels of those example
-    outputs = model(images)
+    model.to(device)
+    outputs = model(images.to(device))
 
     # We got the probability for every 10 labels. The highest (max) probability should be correct label
     _, predicted = torch.max(outputs.data, 1)
@@ -59,13 +53,9 @@ def testBatch():
                                   for j in range(batch_size)))
 
 
-# Function to save the model
-def saveModel():
-    path = "./myFirstModel.pth"
-    torch.save(model.state_dict(), path)
 
 # Function to test the model with the test dataset and print the accuracy for the test images
-def testAccuracy():
+def test_accuracy():
     
     model.eval()
     accuracy = 0.0
@@ -80,21 +70,49 @@ def testAccuracy():
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             accuracy += (predicted == labels.to(device)).sum().item()
-    
+
     # compute the accuracy over all test images
     accuracy = (100 * accuracy / total)
     return accuracy
 
+
+def test_class_accuracy(groups):
+
+    model.to(device)
+    model.eval()
+    n_groups = len(groups)
+    group_total = [0] * n_groups
+    group_accuracy = []
+    total = [0] * n_groups
+
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+
+            # run the model on the test set to predict labels
+            outputs = model(images.to(device))
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            test = (predicted==labels.squeeze())
+            for n, item in enumerate(labels):
+                item_group = item.item()
+                total[item_group] += 1
+                if test[n].item()==true:
+                    group_total[item_group] += 1
+
+        for n,group in enumerate(classes):
+            group_accuracy.append((group_total[n]/total[n])*100)
+            print(group+" ("+str(n)+") accuracy: "+str(round(group_accuracy[n],2))+"%")
+
+
 def train(num_epochs):
     model.train()
-    best_accuracy = 0.0
-    
+
     # Convert model parameters and buffers to CPU or Cuda
     model.to(device)
 
     for epoch in range(num_epochs):  # loop over the dataset multiple times
         running_loss = 0.0
-        running_acc = 0.0
         start_time = time.time()
         for i, (images, labels) in enumerate(train_loader, 0):
             # get the inputs
@@ -117,23 +135,23 @@ def train(num_epochs):
             running_loss += loss.item()     # extract the loss value
             if i % int((n_batches - 1)/2) == int((n_batches - 1)/2 - 1):    
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / (n_batches)))
+                      (epoch + 1, i + 1, running_loss / n_batches))
                 # zero the loss
                 running_loss = 0.0
 
         # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
-        accuracy = testAccuracy()
+        accuracy = test_accuracy()
         print('For epoch', epoch+1,'the test accuracy over the whole test set is %d %%' % accuracy,"(",round((time.time() - start_time),3),"seconds)")
         
         # we want to save the model if the accuracy is the best
-        if accuracy > best_accuracy:
-            saveModel()
-            best_accuracy = accuracy
+        """if accuracy > best_accuracy:
+            save_model()
+            best_accuracy = accuracy"""
 
-        #print("--- %s seconds ---" % )
-
+    torch.save(model.state_dict(), "./myFirstModel.pth")
 # Define your execution device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 print("The model will be running on", device, "device")
 
@@ -158,17 +176,20 @@ optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
 example,label= next(iter(train_loader))
 # Let's build our model
-#train(5)
-print('Finished Training')
+#train(10)
+#print('Finished Training')
+
+
 
 # Let's load the model we just created and test the accuracy per label
 path = "myFirstModel.pth"
 model.load_state_dict(torch.load(path))
 print('Loaded model')
 
+test_class_accuracy(classes)
 
 # Test with batch of images
-testBatch()
+test_batch()
 
 
 """
