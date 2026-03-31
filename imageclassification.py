285c859866611e61c18d8f33ee9c99bc3bb72917
jason <jason_morgan84@yahoo.co.uk>
2026-03-31 15:30:22 +0100

    fixed captum visualisation

--- imageclassification.py
+++ imageclassification.py
@@ -9,16 +9,16 @@ from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.autograd import Variable
 import time
-#from captum.attr import Occlusion
+from captum.attr import Occlusion
+import numpy as np
 
-#-1: Fix artifacts in outputted images?
 
+
+
+#-1: what does captum output mean?
 #0: look into loss, training loss v validation loss
-#1: add section to testAccuracy to return accuracy by group
-
-
+#1: Fix artifacts in outputted images?
 #2 look into more confocal specific models
-
 #3 how to choose the cnn architecture?
 
 
@@ -108,7 +108,6 @@ def test_class_accuracy(groups):
 def train(num_epochs):
     model.train()
 
-    # Convert model parameters and buffers to CPU or Cuda
     model.to(device)
 
     for epoch in range(num_epochs):  # loop over the dataset multiple times
@@ -150,8 +149,8 @@ def train(num_epochs):
 
     torch.save(model.state_dict(), "./myFirstModel.pth")
 # Define your execution device
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cpu")
 
 print("The model will be running on", device, "device")
 
@@ -186,61 +185,52 @@ path = "myFirstModel.pth"
 model.load_state_dict(torch.load(path))
 print('Loaded model')
 
-test_class_accuracy(classes)
+#test_class_accuracy(classes)
 
 # Test with batch of images
-test_batch()
+#test_batch()
 
 
-"""
+
 model.eval()
 
 occlusion = Occlusion(model)
-strides = (1,5, 5)               # smaller = more fine-grained attribution but slower
+strides = (3,9, 9)               # smaller = more fine-grained attribution but slower
 target=0,                       # AR index
-sliding_window_shapes=(1,10, 10)  # choose size enough to change object appearance
-baselines = 0                     # values to occlude the image with. 0 corresponds to gray
+sliding_window_shapes=(3,15, 15)  # choose size enough to change object appearance
+baselines = 0                     # values to occlude the image with 0 corresponds to gray
 
 interpretation_loader=DataLoader(test_images, batch_size=1, shuffle=True, num_workers=0)
 example ,label= next(iter(interpretation_loader))
 #example = test_images[2][0]
-print(label)
 
 
 image_output = np.transpose(example.squeeze().cpu().detach().numpy(), (1,2,0))
+image_output+=1
+print(image_output.max(),image_output.min())
+
 attribution = occlusion.attribute(example,
                                        strides = strides,
                                        target=target,
                                        sliding_window_shapes=sliding_window_shapes,
                                        baselines=baselines)
 
-
+print("Attribution complete")
 
 from captum.attr import visualization as viz
 # Convert the compute attribution tensor into an image-like numpy array
 attribution_output = np.transpose(attribution.squeeze().cpu().detach().numpy(), (1,2,0))
-
-new_attribution_output = np.empty([1,256,256])
-new_image_output = np.empty([1,256,256])
-
-for n,row in enumerate(attribution_output):
-    for m,column in enumerate(row):
-        new_attribution_output[0][n][m] = column[0] + column[1]
-        new_image_output[0][n][m] = column[0] + column[1]
-new_attribution_output = np.transpose(new_attribution_output, (1,2,0))
-new_image_output = np.transpose(new_image_output, (1,2,0))
-#print(new_output.shape)
 
 
 vis_types = ["heat_map", "original_image"]
 vis_signs = ["all", "all"] # "positive", "negative", or "all" to show both
 # positive attribution indicates that the presence of the area increases the prediction score
 # negative attribution indicates distractor areas whose absence increases the score
-print(attribution_output.shape)
-print(image_output.shape)
-_ = viz.visualize_image_attr_multiple(new_attribution_output,
-                                      new_image_output,
+
+"""_ = viz.visualize_image_attr_multiple(attribution_output,
+                                      image_output,
                                       vis_types,
                                       vis_signs,
-                                      show_colorbar = True)
-"""
+                                      show_colorbar = True)"""
+
+viz.visualize_image_attr_multiple(attribution_output,original_image=image_output,signs=["all", "positive", "negative"],methods=["original_image", "blended_heat_map","blended_heat_map"])
