be839691eb48e468cdb7a9f25378b386e17d1149
jason <jason_morgan84@yahoo.co.uk>
2026-04-01 13:40:19 +0100

    split interpretation function

--- image_classification.py
+++ image_classification.py
@@ -2,20 +2,16 @@ import torch
 import classification_class
 from torchvision.transforms import Compose,Normalize
 from torch.utils.data import DataLoader
-from captum.attr import Occlusion
-import numpy as np
 
 import model_test
 import model_train
-
-
+import model_interpret
 
 #-1: what does captum output mean?
 #0: look into loss, training loss v validation loss
 #1: Fix artifacts in outputted images?
 #2 look into more confocal specific models
 #3 how to choose the cnn architecture?
-
 
 batch_size = 5
 classes = ("AR","ARS")
@@ -40,14 +36,9 @@ test_loader=DataLoader(train_images, batch_size=batch_size, shuffle=True, num_wo
 
 model = classification_class.Network()
 
-
-
-
-example,label= next(iter(train_loader))
 # Let's build our model
 #model_train.train(model, device, train_loader, test_loader, 10)
 print('Finished Training')
-
 
 
 # Let's load the model we just created and test the accuracy per label
@@ -60,47 +51,7 @@ print('Loaded model')
 # Test with batch of images
 #test_batch()
 
+model_interpret.model_occlusion(model,test_images)
 
 
-model.eval()
 
-occlusion = Occlusion(model)
-strides = (3,9, 9)               # smaller = more fine-grained attribution but slower
-target=0,                       # AR index
-sliding_window_shapes=(3,15, 15)  # choose size enough to change object appearance
-baselines = 0                     # values to occlude the image with 0 corresponds to gray
-
-interpretation_loader=DataLoader(test_images, batch_size=1, shuffle=True, num_workers=0)
-example ,label= next(iter(interpretation_loader))
-#example = test_images[2][0]
-
-
-image_output = np.transpose(example.squeeze().cpu().detach().numpy(), (1,2,0))
-image_output+=1
-print(image_output.max(),image_output.min())
-
-attribution = occlusion.attribute(example,
-                                       strides = strides,
-                                       target=target,
-                                       sliding_window_shapes=sliding_window_shapes,
-                                       baselines=baselines)
-
-print("Attribution complete")
-
-from captum.attr import visualization as viz
-# Convert the compute attribution tensor into an image-like numpy array
-attribution_output = np.transpose(attribution.squeeze().cpu().detach().numpy(), (1,2,0))
-
-
-vis_types = ["heat_map", "original_image"]
-vis_signs = ["all", "all"] # "positive", "negative", or "all" to show both
-# positive attribution indicates that the presence of the area increases the prediction score
-# negative attribution indicates distractor areas whose absence increases the score
-
-"""_ = viz.visualize_image_attr_multiple(attribution_output,
-                                      image_output,
-                                      vis_types,
-                                      vis_signs,
-                                      show_colorbar = True)"""
-
-viz.visualize_image_attr_multiple(attribution_output,original_image=image_output,signs=["all", "positive", "negative"],methods=["original_image", "blended_heat_map","blended_heat_map"])
