85f7d55700c322fe1e99422c1bcf283230e7765f
jason <jason_morgan84@yahoo.co.uk>
2026-04-03 15:24:47 +0100

    fixed bug in testing accuracy during training

--- image_classification.py
+++ image_classification.py
@@ -7,6 +7,7 @@ import model_interpret
 import model_test
 import model_train
 
+#0: more training data
 #1: what does captum output mean?
 ########: 1.2: Allow testing/interepretation of chosen images
 ########: 1.3: Test other methods of interpretation
@@ -16,7 +17,8 @@ import model_train
 #4 look into more confocal specific models
 #5 how to choose the cnn architecture?
 
-batch_size = 5
+training_batch_size = 10
+test_batch_size = 5
 classes = ("AR","ARS")
 
 # Define your execution device
@@ -27,7 +29,7 @@ print("The model will be running on", device, "device")
 transform_norm = Compose([Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_images = classification_class.ImagesDataset(annotations_file='images.csv', img_dir='samples/',transform=transform_norm)
-n_batches = len(train_images)/batch_size
+#n_batches = len(train_images)/batch_size
 
 test_images = classification_class.ImagesDataset(annotations_file='test_images.csv', img_dir='test/',transform=transform_norm)
 
@@ -36,9 +38,9 @@ model = classification_class.Network()
 
 
 # Let's build our model
-train_loader=DataLoader(train_images, batch_size=batch_size, shuffle=True, num_workers=0)
-test_loader=DataLoader(test_images, batch_size=batch_size, shuffle=True, num_workers=0)
-model_train.train(model, device, train_loader, test_loader, 10)
+train_loader=DataLoader(train_images, batch_size=training_batch_size, shuffle=True, num_workers=0)
+test_loader=DataLoader(test_images, batch_size=test_batch_size, shuffle=True, num_workers=0)
+model_train.train(model, device, train_loader, test_loader, 20)
 print('Finished Training')
 
 
@@ -55,15 +57,18 @@ print(file_name[0],"is",classes[label.item()],"and is predicted to be",classes[p
 
 
 # Get accuracy of classes:
-test_loader=DataLoader(test_images, batch_size=batch_size, shuffle=True, num_workers=0)
+test_loader=DataLoader(test_images, batch_size=test_batch_size, shuffle=True, num_workers=0)
 model_test.test_class_accuracy(model, device, test_loader, classes)
 
 # Test batch of images
-test_loader=DataLoader(test_images, batch_size=batch_size, shuffle=True, num_workers=0)
+test_loader=DataLoader(test_images, batch_size=test_batch_size, shuffle=True, num_workers=0)
 images, labels, file_names = next(iter(test_loader))
 prediction = model_test.test_item(model, device, images)
-print('Real labels: ', ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
-print('Predicted: ', ' '.join('%5s' % classes[prediction[j]] for j in range(batch_size)))
+print('Real labels: ', ' '.join('%5s' % classes[labels[j]] for j in range(test_batch_size)))
+print('Predicted: ', ' '.join('%5s' % classes[prediction[j]] for j in range(test_batch_size)))
+
+
+
 #model_interpret.model_occlusion(model, image)
 
 
